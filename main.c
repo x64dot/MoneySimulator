f92ef2a2e21b9562317206eb2a824af4b241bbd8
x64dot <142705403+x64dot@users.noreply.github.com>
2024-04-20 11:30:11 -0400

    Update main.c
    Added support for rebirths.

--- main.c
+++ main.c
@@ -9,20 +9,21 @@
 #include <ctype.h>
 #include <errno.h>
 
-int check_dir();
-void create();
-void save(int cash);
-int load_cash();
-void help();
-int beg();
-int hack();
-int balance(int balance);
-void lower_string();
-
 struct PlayerData{
     int cash;
-    int rebirths; /* Coming soon */
+    int rebirths;
 };
+
+int check_dir();
+void create();
+void save(int cash, int rebirths);
+int load_cash(int *rebirths);
+void help();
+int beg(int rebirths);
+int hack(int rebirths);
+int balance(int balance);
+void lower_string();
+void rebirth(struct PlayerData* player);
 
 int main(){
     char input[10];
@@ -35,9 +36,10 @@ int main(){
     if(check_dir() == -1){
         create();
         player->cash = 0;
+        player->rebirths = 0;
     }
     else{
-        player->cash = load_cash();
+        player->cash = load_cash(&(player->rebirths));
     }
 
     while (1){
@@ -59,56 +61,45 @@ int main(){
 
         if (strcmp(input, "help") == 0){
             help();
-
-            save(player->cash);
+            save(player->cash, player->rebirths);
         }
         else if (strcmp(input, "beg") == 0){
             usleep(4000000);
-            track = beg();
+            track = beg(player->rebirths);
             player->cash += track;
-            printf("you earned %d from begging!\n", track);
-
-            save(player->cash);
-
+            printf("You earned %d from begging!\n", track);
+            save(player->cash, player->rebirths);
         }
         else if (strcmp(input, "balance") == 0 || strcmp(input, "bal") == 0){
             track = balance(player->cash);
-
             printf("Cash balance %d\n", track);
-
-            save(player->cash);
+            save(player->cash, player->rebirths);
         }
         else if (strcmp(input, "hack") == 0){
             usleep(8000000);
-
-            track = hack();
-
-            if (track == 100){
+            track = hack(player->rebirths);
+            if (track == 100 * (player->rebirths + 1)){
                 player->cash -= 100;
-                puts("You have lost 100 money, due to the fail of the hack.");
+                puts("You have lost 100 money, due to the failure of the hack.");
             }
             else{
                 player->cash += track;
             }
-            
-
-            save(player->cash);
-
+            save(player->cash, player->rebirths);
+        }
+        else if (strcmp(input, "rebirth") == 0){
+            rebirth(player);
         }
         else if (strcmp(input, "exit") == 0){
-            save(player->cash);
-
+            save(player->cash, player->rebirths);
             free(player);
             return 0;
         }
         else {
-            save(player->cash);
             puts("Invalid command.");
+            save(player->cash, player->rebirths);
         }
-  
     }
-
-
 
     free(player);
 
@@ -149,49 +140,45 @@ void create() {
     return;
 }
 
-
-void save(int cash){
+void save(int cash, int rebirths){
     FILE* fptr;
-    char *money;
+    char *data;
 
-    money = (char *)malloc(sizeof(char) * 30);
-    if (money == NULL) {
+    data = (char *)malloc(sizeof(char) * 30);
+    if (data == NULL) {
         return;
     }
 
+    sprintf(data, "%d %d", cash, rebirths);
 
-    sprintf(money, "%d", cash);
-
-    
     fptr = fopen("/.SimData/data.txt", "w");
     if (fptr == NULL) {
-        free(money);
+        free(data);
         return;
     }
 
-    
-    if (fprintf(fptr, "%s", money) < 0) {
-        free(money);
+    if (fprintf(fptr, "%s", data) < 0) {
+        free(data);
         fclose(fptr);
         return;
     }
 
     fclose(fptr);
-    free(money);
-
+    free(data);
 }
 
-int load_cash(){
+int load_cash(int *rebirths){
     FILE* fptr;
 
     fptr = fopen("/.SimData/data.txt", "r");
 
     if (fptr == NULL){
+        *rebirths = 0;
         return 0;
     }
 
     int cash;
-    fscanf(fptr, "%d", &cash);
+    fscanf(fptr, "%d %d", &cash, rebirths);
     
     fclose(fptr);
 
@@ -204,22 +191,24 @@ void help(){
     puts("Command: \"beg\" to earn money");
     puts("Command: \"hack\" to earn money but with the risk of losing money.");
     puts("Command: \"balance/bal\" to check your balance");
+    puts("Command: \"rebirth\" to be reborned, and earn more.");
 
     puts("---------------------------");
 }
 
-int beg(){
-    return (rand() % 30) + 1;
+int beg(int rebirths){
+    int base_cash = (rand() % 30) + 1;
+    return base_cash * (rebirths + 1);
 }
 
-int hack(){
+int hack(int rebirths){
     int random = rand() % 51; 
     random += 50;
 
     char* arrm[] = {"You have hacked a random stranger successfully!",
                    "You have hacked the FBI, and stole a good portion of their money!"};
-    char* arrc[] = {"You have been caught by the FBI and have been forced to pay 100 in cash.",
-                    "You have been caught by the NSA and have been forced to pay 100 in cash."};
+    char* arrc[] = {"You have been caught by the FBI and have been forced to pay.",
+                    "You have been caught by the NSA and have been forced to pay."};
 
     for (int i = 2; i * i <= random; i++) {
         if (random % i == 0) {
@@ -229,14 +218,13 @@ int hack(){
 
             if (rand_int == 0){
                 printf("%s\n", arrm[0]);
-                return random;
+                return (random * (rebirths + 1));
             }
             else{
                 printf("%s\n", arrm[1]);
-                return random;
+                return (random * (rebirths + 1));
             }
         }
-
     }
 
     srand(time(NULL));
@@ -244,8 +232,7 @@ int hack(){
 
     printf("%s\n", arrc[rand_int]);
 
-    return 100;
-    
+    return 100 * (rebirths + 1);
 }
 
 int balance(int balance){
@@ -256,6 +243,41 @@ void lower_string(char *str){
     for (int i = 0; str[i]; i++){
         str[i] = tolower(str[i]);
     }
+}
 
-    return;
+void rebirth(struct PlayerData* player) {
+    int base_cost = 500;
+    int cost = base_cost * (player->rebirths + 1);
+
+    char input[4];
+    while (1) {
+        printf("Are you sure you want to rebirth? This will cost %d cash. (yes/no): ", cost);
+
+        int c;
+        while ((c = getchar()) != '\n' && c != EOF) {}
+
+        if (fgets(input, sizeof(input), stdin) == NULL) {
+            printf("Error: Input too long.\n");
+            return;
+        }
+
+        input[strcspn(input, "\n")] = '\0';
+        lower_string(input);
+
+        if (strcmp(input, "yes") == 0) {
+            if (player->cash >= cost) {
+                player->cash -= cost;
+                player->rebirths++;
+                printf("Congratulations! You have been reborn. Your rebirth count is now %d.\n", player->rebirths);
+            } else {
+                printf("You don't have enough cash to rebirth.\n");
+            }
+            break; 
+        } else if (strcmp(input, "no") == 0) {
+            printf("Rebirth aborted.\n");
+            break; 
+        } else {
+            printf("Invalid input. Please enter \"yes\" or \"no\".\n");
+        }
+    }
 }
