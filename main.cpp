0e6a0d678a6afe1718310f5a94f3d2c230f8909b
x64dot <142705403+x64dot@users.noreply.github.com>
2024-07-31 13:19:22 -0400

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <string>
+#include "core.h"
+
+using namespace std;
+
+int main(){
+  
+  MoneyOp MoneyObj;
+  Functionality FuncObj;
+
+  PlayerData* player = new (nothrow) PlayerData;
+  if (player == nullptr){
+      cout << "Memory allocation failed.";
+  }
+
+  player->money = 0;
+  player->gems = 0;
+  player->boost = 0;
+
+  while (1){
+    string input;
+    cout << "What will be your input? ";
+    
+    getline(cin, input);
+
+    if (input == "exit"){
+        delete player;
+        return 0;
+    }
+    else if (input == "bal"){
+        MoneyObj.getbal(player); 
+    }
+    else if (input == "beg"){
+       MoneyObj.beg(player); 
+    }
+    else if (input == "clear"){
+       FuncObj.clear();     
+    }      
+    else{
+       cout << "Invalid Input: " << input << "\n";
+    }
+  }
+
+
+  return 0;
+}
